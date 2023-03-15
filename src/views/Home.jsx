e8698042d8b140eb65071078a4a6bc3c1a4962fc
Ibai Farina <ibaifo8@gmail.com>
2023-03-15 17:01:35 +0100

    typewriter

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Typewriter from 'typewriter-effect';
 import {Subtitle, TitleBase} from "../components/UI/Texts";
 import Navbar from "../components/Navbar";
 
@@ -7,7 +8,7 @@ const HeroContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: calc(60vh);
+  height: 100vh;
   background: #000249;
   text-align: left;
   margin: 0;
@@ -37,21 +38,45 @@ const HeroTitle = styled.h1`
   }
 `;
 
-const HeroSubtitle = styled(Subtitle)`
-  font-size: 4em;
-  //width: 50%;
+const Console = styled.p`
+  font-size: 1.5em;
+  text-align: left;
+  display: flex;
+  background: rgba(0, 0, 0, 0.6);
+  padding: 5px 15px;
+  border-radius: 10px;
+  width: fit-content;
+  margin-left: auto;
+  margin-right: auto;
   text-shadow: 0 10px 10px rgba(0, 0, 0, 0.12);
+
+  & .console__typewrite {
+    color: rgba(245, 245, 245, 0.9);
+    margin-left: 5px;
+  }
 `;
 
 
 const Home = () => {
     return (
         <div id="home" name="home">
-            <Navbar/>
+            {/*<Navbar/>*/}
             <HeroContainer>
                 <div className="text-center b">
                     <HeroTitle>Ibai Farina</HeroTitle>
-                    <HeroSubtitle>イバイファリナ</HeroSubtitle>
+                    <Console>
+                        <span className="text-green-400">></span>
+                        <div className="console__typewrite">
+                            <Typewriter
+                                options={{
+                                    strings: ["Python developer.", "Node.js developer."],
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            />
+                        </div>
+                    </Console>
+
                 </div>
             </HeroContainer>
         </div>
